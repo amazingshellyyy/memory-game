e5b67222a73791f6d34422877b0bacfe2a328895
amazingshellyyy <amazingshellyyy@gmail.com>
2019-12-07 23:50:30 -0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,14 +1,24 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let cardOne = cards[0];
-let cardTwo = cards[1];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardTwo);
-if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You find a match!");
-} else {
-    alert("Sorry, try again!");
+function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+        alert("You find a match!");
+    } else {
+        alert("Sorry, try again!");
+    }
 }
+
+function flipCard(cardId) {
+    console.log("user flipped " + cards[cardId]);
+    cardsInPlay.push(cards[cardId]);
+    console.log(cardsInPlay);
+    if (cardsInPlay.length == 2) {
+        checkForMatch();
+    }
+
+
+
+}
+flipCard(0);
+flipCard(1);
